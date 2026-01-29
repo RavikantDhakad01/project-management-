725652048d8678b06dfccd716b30c2e9878e82a8
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-01-29 10:28:33 +0530

    config & cors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
-import express from "express"
+import express from "express";
+import cors from "cors";
 
-const app =express()
+const app = express();
 
-app.get("/",(req,res)=>{
-    res.send("This is home page")
-})
-app.get("/login",(req,res)=>{
-    res.end("This is login page")
-})
-export default app
+//middlewares
+
+app.use(express.json({ limit: "16kb" }));
+
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+app.use(express.static("public"));
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+
+app.get("/", (req, res) => {
+  res.send("This is home page");
+});
+app.get("/login", (req, res) => {
+  res.end("This is login page");
+});
+export default app;
